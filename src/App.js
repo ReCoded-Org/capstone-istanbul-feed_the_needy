4008fdccf70562ec88f245614979573888bb2d05
bertacsevercan <bertac.severcan@gmail.com>
2020-10-24 19:34:45 +0300

    add router and general website layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
+import "antd/dist/antd.css";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import HomePage from "./containers/HomePage";
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Layout className="layout">
+        <NavBar />
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+            <Route path="/home" component={HomePage} />
+            <Route path="/coupons" component="#" />
+            <Route path="/blog" component="#" />
+            <Route path="/admin" component="#" />
+            <Route path="/cart" component="#" />
+          </div>
+        </Content>
+        <Footer />
+      </Layout>
+    </Router>
   );
 }
 
