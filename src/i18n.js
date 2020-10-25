2905d85d27a994481ff70fc119134b6c9ae1fce1
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-25 19:03:45 +0300

    fix imported modules pathes

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import arTranslation from "../public/locales/ar/translation.json";
-import trTranslation from "../public/locales/tr/translation.json";
-import enTranslation from "../public/locales/en/translation.json";
+import arTranslation from "./locales/ar/translation.json";
+import trTranslation from "./locales/tr/translation.json";
+import enTranslation from "./locales/en/translation.json";
 
 i18n
   .use(LanguageDetector)
