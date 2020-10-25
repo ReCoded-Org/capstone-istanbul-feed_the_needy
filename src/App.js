ba4445d6f8c405009feca5dfc057c372f61a8594
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-25 18:17:05 +0300

    wrap App component in I18nextProvider tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
 import HomePage from "./containers/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <div className="App">
+        <HomePage />
+      </div>
+    </I18nextProvider>
   );
 }
 
