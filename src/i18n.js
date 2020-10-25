cc0bb7e9858a6bd8e64dca32161862e350062eb9
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-25 18:56:39 +0300

    import and add languages in i18n config options

--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,25 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import arTranslation from "../public/locales/ar/translation.json";
+import trTranslation from "../public/locales/tr/translation.json";
+import enTranslation from "../public/locales/en/translation.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      ar: {
+        translation: arTranslation,
+      },
+      tr: {
+        translation: trTranslation,
+      },
+      en: {
+        translation: enTranslation,
+      },
+    },
     fallbackLng: "en",
     debug: true,
     interpolation: {
