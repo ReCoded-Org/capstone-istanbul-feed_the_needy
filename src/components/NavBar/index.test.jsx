db96cd7a628f2904d989752869a40a500b882316
bertacsevercan <bertac.severcan@gmail.com>
2020-10-26 16:52:57 +0300

    add snapshot test for navbar

--- src/components/NavBar/index.test.jsx
+++ src/components/NavBar/index.test.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import NavBar from './index';
+
+it('renders correctly', () => {
+    const tree = renderer
+      .create(<NavBar />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
