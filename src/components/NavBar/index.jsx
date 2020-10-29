b0c41faa1cbdef937dad4f9a53521154b9676ede
Bertaç Severcan <62285626+bertacsevercan@users.noreply.github.com>
2020-10-29 11:56:50 +0300

    Navbar (#20)
    * create admin.svg
    
    * create cart.svg
    
    * add style.css for navbar
    
    * add elements to navbar
    
    * add dependency for antd icons
    
    * create image folder under src and add logo.svg
    
    * add foodOnCoupon.svg
    
    * add stackedTitle.svg
    
    * run yarn fix
    
    * import translation hook
    
    * add navbar header translations
    
    * add navbar header tr translations
    
    * add translated headers to navlinks
    
    * change the order of headers in translations
    
    * navbar done
    
    * create test.jsx for navbar
    
    * add snapshot test for navbar
    
    * add react-test-renderer dependency for snapshot tests
    
    * add snapshot test for navbar
    
    * run yarn fix
    
    * add a workaround to jest test to ignore findDomNode warning
    
    * navbar test done
    
    * delete extra spaces
    
    * add  'navbar' to custom classes
    
    * change classNames in index
    
    * add 'navbar' to cart and admin classes
    
    * remove unnecessary icon components
    
    * add necessarry changes as requested
    
    * update snapshot test
    
    * add spaces between each style rule

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Icon from "@ant-design/icons";
+import { Layout, Menu } from "antd";
+import { useTranslation } from "react-i18next";
+import { ReactComponent as AdminSvg } from "./images/admin.svg";
+import { ReactComponent as CartSvg } from "./images/cart.svg";
+import { ReactComponent as LogoSvg } from "../../images/logo.svg";
+import { ReactComponent as FoodOnCouponSvg } from "../../images/foodOnCoupon.svg";
+import { ReactComponent as StackedTitleSvg } from "../../images/stackedTitle.svg";
+import "./style.css";
 
-const NavBar = () => {
-  return <div>Navbar here!</div>;
+const { Header } = Layout;
+
+const NavBar = ({ isTesting }) => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Header className="navbarHeader">
+        <div className="logo">
+          <LogoSvg />
+        </div>
+        <div className="navbarTitle">
+          <FoodOnCouponSvg />
+        </div>
+        <div className="navbarStackedTitle">
+          <StackedTitleSvg />
+        </div>
+        <Menu
+          style={{ background: "#F0B32C" }}
+          theme="dark"
+          mode={isTesting ? "vertical" : "horizontal"}
+          defaultSelectedKeys={["1"]}
+        >
+          <Menu.Item key="1">
+            <Link to="/">{t("navbar.headers.header0")}</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/coupons">{t("navbar.headers.header1")}</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/blog">{t("navbar.headers.header2")}</Link>
+          </Menu.Item>
+          <Menu.Item key="4" className="navbarCart">
+            <Link to="/cart">
+              <Icon style={{ transform: "scale(2)" }} component={CartSvg} />
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="5" className="navbarAdmin">
+            <Link to="/admin">
+              <Icon style={{ transform: "scale(2)" }} component={AdminSvg} />
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+    </>
+  );
 };
 
 export default NavBar;
---
+++ src/components/NavBar/index.test.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import renderer from "react-test-renderer";
+import NavBar from "./index";
+
+jest.mock("react-i18next", () => ({
+  useTranslation: () => ({ t: (key) => key }),
+}));
+
+const isTesting = true;
+
+it("Navbar renders correctly", () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <NavBar isTesting={isTesting} />
+      </Router>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
