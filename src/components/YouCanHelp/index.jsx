f5d929c5d729a85bb7fcba1f479188eb45654594
Gizem Deniz <61707093+gizemcdeniz@users.noreply.github.com>
2020-11-03 22:22:58 +0300

    youcanhelp Section (#18)
    * youcanhelp section completed
    
    * youcanhelp section complete
    
    * responsive design add
    
    * design change
    
    * export default add
    
    * image change
    
    * image change
    
    * test file added
    
    * youcanhelp section completed
    
    * responsive design add
    
    * design change
    
    * image change
    
    * image change
    
    * test file added
    
    * translate file added
    
    * conflict solve
    
    * translation file fix
    
    * conflict solved
    
    * test file add
    
    * test file added
    
    * conflict
    
    * conflict  solve
    
    * fixed test
    
    * package-lock-json delete
    
    * conflict solve
    
    * design change
    
    * syling change
    
    * row & col add
    
    * run fix
    
    * classname added
    
    * yarn test updated
    
    * test updated
    
    * space remove from translate file
    
    * tag change as a classname
    
    * image fix
    
    * conflict solve
    
    * conflict solve
    
    * commented code remove
    
    * commented code remove
    
    Co-authored-by: Halit Batur <61846570+halitbatur@users.noreply.github.com>

--- src/components/YouCanHelp/index.jsx
+++ src/components/YouCanHelp/index.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import HelpImage from "./image.png";
+import { Button, Row, Col } from "antd";
+import { useTranslation } from "react-i18next";
+import "antd/dist/antd.css";
+import "./index.css";
 
-const YouCanHelp = () => {
-  return <div>YouCanHelp here!</div>;
+const YouCanHelp = ({ isTesting }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="containerYoucanhelp">
+      <Row span={36} xs={24} sm={20} md={36} xl={30} justify="space-between">
+        <Col
+          className="youCanHelpToo"
+          span={16}
+          xs={24}
+          sm={20}
+          md={24}
+          xl={10}
+        >
+          <Col className="youCanHelpText">
+            <h1>You can help too!</h1>
+            <span>{t("youcanhelp.headers.textSection1")}</span>
+            <Col>
+              {isTesting ? (
+                <button>Donate</button>
+              ) : (
+                <Button className="donateButton">
+                  {t("youcanhelp.headers.button")}
+                </Button>
+              )}
+            </Col>
+          </Col>
+        </Col>
+        <Col className="imageSection" span={36} xs={8} sm={20} md={24} xl={12}>
+          <img
+            className="foodImage"
+            span={36}
+            xs={8}
+            sm={20}
+            md={24}
+            xl={12}
+            src={HelpImage}
+            alt="help"
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 };
-
 export default YouCanHelp;
