6ecdb969cf8c61b70a8072d981a159121b29b256
Cihan Uygur <54838188+cuygur@users.noreply.github.com>
2020-11-03 12:45:45 +0300

    Our sponsors comp (#29)
    * add svg files
    
    * import svg files to index
    
    * add cards
    
    * delete cards
    
    * add translation
    
    * style svg files
    
    * fix title text
    
    * add test file
    
    * fix test file
    
    * yarn run test success
    
    * yarn run fix
    
    * change classnames in index
    
    * fix classnames in css
    
    * change translation key
    
    * yarn run fix
    
    * delete snapshot folder
    
    * yarn run test success
    
    * change classnames in index
    
    * change classnames in css
    
    * change object keys
    
    * fix for snapshot
    
    * snapshot test success
    
    * yarn run fix
    
    * yarn run fix
    
    * change classnames in index
    
    * change classnames in css
    
    * yarn run fix
    
    * delete test
    
    * yarn run fix
    
    * fix package.json
    
    * delete test
    
    * fix conflict
    
    * yarn run test
    
    * delete style
    
    * delete test
    
    * yarn run test
    
    * fix conflict
    
    * yarn run fix

--- src/components/OurSponsors/index.jsx
+++ src/components/OurSponsors/index.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "antd/dist/antd.css";
+import "./style.css";
+import { useTranslation } from "react-i18next";
+import { Row, Col, Typography } from "antd";
+import { ReactComponent as BurgerKing } from "./BurgerKing.svg";
+import { ReactComponent as McDonalds } from "./McDonalds.svg";
+import { ReactComponent as Tesco } from "./Tesco.svg";
+import { ReactComponent as PizzaHut } from "./PizzaHut.svg";
+
+const { Title } = Typography;
 
 const OurSponsors = () => {
-  return <div>OurSponsors here!</div>;
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <Row gutter={16} className="ourSponsorsTitle">
+        <Title>{t("ourSponsorsComp.card.title0")}</Title>
+      </Row>
+      <Row gutter={16} justify="space-around">
+        <Col xs={24} sm={24} md={12} xl={6}>
+          <BurgerKing className="burgerKing" />
+        </Col>
+        <Col xs={24} sm={24} md={12} xl={6}>
+          <McDonalds />
+        </Col>
+        <Col xs={24} sm={24} md={12} xl={6}>
+          <Tesco className="tesco" />
+        </Col>
+        <Col xs={24} sm={24} md={12} xl={6}>
+          <PizzaHut />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default OurSponsors;
