380dd80cfe7feeb515261e0dc1927fd4e70c6087
Bertaç Severcan <62285626+bertacsevercan@users.noreply.github.com>
2020-11-19 17:02:26 +0300

    Chart (#47)
    * create couponsChart
    
    * add chartjs dependency
    
    * add example chart
    
    * fetch soldCoupons from db
    
    * add statistics charts
    
    * add responsiveness to bar chart
    
    * refactor code
    
    * make texts smaller for card in mobile
    
    * add key to col
    
    * add style to spin and update test
    
    * run yarn fix
    
    * add responsive layout to statistics
    
    * run yarn fix
    
    * fix translation
    
    * add translation for spin
    
    * run yarn fix
    
    * update test

--- src/containers/AdminDashboard/index.jsx
+++ src/containers/AdminDashboard/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CouponsChart from "../../components/CouponsChart";
 import CreateCoupons from "../../components/CreateCoupons";
 import EditOrganization from "../../components/EditOrganization";
 
 const AdminDashboard = () => {
   return (
     <div>
+      <CouponsChart />
       <CreateCoupons />
       <EditOrganization />
     </div>
