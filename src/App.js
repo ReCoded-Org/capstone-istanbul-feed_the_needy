f543c6cf909d9fc83407762081bbb408bc446b45
Cihan Uygur <54838188+cuygur@users.noreply.github.com>
2020-11-20 10:47:58 +0300

    add coupon container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
-import "./App.css";
-import { I18nextProvider } from "react-i18next";
-import "antd/dist/antd.css";
-import { Layout } from "antd";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import i18n from "./i18n";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import HomePage from "./containers/HomePage";
-import CouponsPage from "./containers/CouponsPage";
-import AdminDashboard from "./containers/AdminDashboard";
+import React from "react"
+import "./App.css"
+import { I18nextProvider } from "react-i18next"
+import "antd/dist/antd.css"
+import { Layout } from "antd"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import i18n from "./i18n"
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+import HomePage from "./containers/HomePage"
+import CouponsPage from "./containers/CouponsPage"
+import AdminDashboard from "./containers/AdminDashboard"
+import Coupon from "./containers/Coupon"
 
-const { Content } = Layout;
+const { Content } = Layout
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <div className="site-layout-content">
               <Route exact path="/" component={HomePage} />
               <Route path="/coupons" component={CouponsPage} />
+              <Route path="/coupon" component={Coupon} />
               <Route path="/blog" component="#" />
               <Route path="/admin" component={AdminDashboard} />
               <Route path="/cart" component="#" />
@@ -32,7 +34,7 @@ function App() {
         </Layout>
       </Router>
     </I18nextProvider>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/Coupon/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "antd/dist/antd.css"
+
+const Coupon = () => {
+  return <div>Coupon</div>
+}
+
+export default Coupon:
