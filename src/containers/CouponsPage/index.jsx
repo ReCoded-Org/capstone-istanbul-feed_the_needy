fa495fd072adebf497ede2b0186ecf4a5561ea51
Bertaç Severcan <62285626+bertacsevercan@users.noreply.github.com>
2020-11-21 18:39:59 +0300

    add sping load (#61)

--- src/containers/CouponsPage/index.jsx
+++ src/containers/CouponsPage/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Typography } from "antd";
+import { Typography, Spin } from "antd";
 import { useTranslation } from "react-i18next";
 import Coupon from "../../components/Coupon";
 import firebase from "../../firebaseConfig";
@@ -11,6 +11,7 @@ const { Title } = Typography;
 const CouponsPage = () => {
   const { t } = useTranslation();
   const [availableCoupons, setAvailableCoupons] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     db.collection("coupons")
@@ -32,6 +33,7 @@ const CouponsPage = () => {
           };
         });
         setAvailableCoupons(modifiedCouponsArray);
+        setLoading(false);
       });
   }, []);
 
@@ -43,17 +45,21 @@ const CouponsPage = () => {
       <Title style={{ textAlign: "center" }} level={1}>
         {t("couponsPage.restaurants")}
       </Title>
-      {availableCoupons.map((coupon) => {
-        return (
-          <Coupon
-            id={coupon.id}
-            createdAt={coupon.createdAt}
-            amounts={coupon.amounts}
-            compName={coupon.compName}
-            img={coupon.img}
-          />
-        );
-      })}
+      {loading ? (
+        <Spin className="spin" size="large" tip={t("spin")} />
+      ) : (
+        availableCoupons.map((coupon) => {
+          return (
+            <Coupon
+              id={coupon.id}
+              createdAt={coupon.createdAt}
+              amounts={coupon.amounts}
+              compName={coupon.compName}
+              img={coupon.img}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
